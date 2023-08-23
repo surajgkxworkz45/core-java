3764ad484d1b8a298b2ccb733470f0bc09242211
@surajgkxworkz45 <surajgk.xworkz@gmail.com>
2023-08-23 10:07:37 +0530

    uploding methodsoverload

---
+++ GrubHubExecuter.java
@@ -0,0 +1,160 @@
+class GrubHubExecuter{
+	public static void main(String args []){
+		double price=0.0;
+		double priceWithQuantity=0.0;
+		
+	priceWithQuantity = GrubHub.search("Fish Thali",3);
+	System.out.println("The priceWithQuantity of Fish Thali is "+  priceWithQuantity);
+	
+	priceWithQuantity = GrubHub.search("yasham Palava",2);
+	System.out.println( "The priceWithQuantity of yasham Palava is " + priceWithQuantity);
+	
+	priceWithQuantity = GrubHub.search("Veg Biriyani",4);
+	System.out.println( "The priceWithQuantity of Veg Biriyani is " + priceWithQuantity);
+	
+	priceWithQuantity = GrubHub.search("Chicken Biriyani",5);
+	System.out.println( "The priceWithQuantity of Chicken Biriyani is " + priceWithQuantity);
+	
+	priceWithQuantity = GrubHub.search("Mutton Biriyani",6);
+	System.out.println("The priceWithQuantity of Mutton Biriyani is " + priceWithQuantity);
+	
+	priceWithQuantity = GrubHub.search("Curry",9);
+	System.out.println("The priceWithQuantity of Curry is  " + priceWithQuantity);
+	
+	priceWithQuantity = GrubHub.search("Mutton Chaps",3);
+	System.out.println( "The priceWithQuantity of Mutton Chaps is " + priceWithQuantity);
+	
+	priceWithQuantity = GrubHub.search("Panner Pasanda",4);
+	System.out.println( "The priceWithQuantity of Panner Pasanda is  " + priceWithQuantity);
+	
+	priceWithQuantity = GrubHub.search("Vegetable Curry",5);
+	System.out.println( "The priceWithQuantity of Vegetable Curry is  " + priceWithQuantity);
+	
+	priceWithQuantity = GrubHub.search("Vegetable Jalfrezi",6);
+	System.out.println( "The priceWithQuantity of Vegetable Jalfrezi is  " + priceWithQuantity);
+	
+	priceWithQuantity = GrubHub.search("Khadi Pakora",10);
+	System.out.println( "The priceWithQuantity of Khadi Pakora is" + priceWithQuantity);
+	
+	priceWithQuantity = GrubHub.search("Panner Curry",11);
+	System.out.println("The priceWithQuantity of Panner Curry is " + priceWithQuantity);
+	
+	priceWithQuantity = GrubHub.search("Panner Fry",1);
+	System.out.println("The priceWithQuantity of Panner Fry is  " + priceWithQuantity);
+	
+	priceWithQuantity = GrubHub.search("Indian Chickpea Curry",4);
+	System.out.println("The priceWithQuantity of Indian Chickpea Curry is  " + priceWithQuantity);
+	
+	priceWithQuantity = GrubHub.search("Bomby Potatoes",6);
+	System.out.println("The priceWithQuantity of Bomby Potatoes is " + priceWithQuantity);
+	
+	priceWithQuantity = GrubHub.search("Chana Masala",7);
+	System.out.println("The priceWithQuantity of Chana Masala is " + priceWithQuantity);
+	
+	priceWithQuantity = GrubHub.search("Gobhi Manchuri",8);
+	System.out.println("The priceWithQuantity of Gobhi Manchuri is " + priceWithQuantity);
+	
+	
+	priceWithQuantity = GrubHub.search("Dal Fry",5);
+	System.out.println("The priceWithQuantity of Dal Fry is " + priceWithQuantity);
+	
+	priceWithQuantity = GrubHub.search("Dal Kichadi",7);
+	System.out.println("The priceWithQuantity of Dal Kichadi is" + priceWithQuantity);
+	
+	priceWithQuantity = GrubHub.search("Mutton Soup",8);
+	System.out.println("The priceWithQuantity of Mutton Soup is " + priceWithQuantity);
+	
+	priceWithQuantity = GrubHub.search("Curd",6);
+	System.out.println("The priceWithQuantity of " + priceWithQuantity);
+	
+	priceWithQuantity = GrubHub.search("Dosa",3);
+	System.out.println("The priceWithQuantity of Dosa is " + priceWithQuantity);
+	
+	priceWithQuantity = GrubHub.search("Paddu",4);
+	System.out.println("The priceWithQuantity of Paddu is " + priceWithQuantity);
+	
+	priceWithQuantity = GrubHub.search("Puri",5);
+	System.out.println("The priceWithQuantity of Puri is  " + priceWithQuantity);
+	
+	priceWithQuantity = GrubHub.search("Pan Fried Panner",6);
+	System.out.println("The priceWithQuantity of Pan Fried Panner is " + priceWithQuantity);
+	
+	priceWithQuantity = GrubHub.search("Veg Kurma ",7);
+	System.out.println("The priceWithQuantity of Veg Kurma is " + priceWithQuantity);
+	
+	priceWithQuantity = GrubHub.search("Kaju Fry",8);
+	System.out.println("The priceWithQuantity of Kaju Fry is " + priceWithQuantity);
+	
+	priceWithQuantity = GrubHub.search("Sambara",9);
+	System.out.println("The priceWithQuantity of Sambara is " + priceWithQuantity);
+	
+	priceWithQuantity = GrubHub.search("Malai Kofta",3);
+	System.out.println("The priceWithQuantity of Malai Kofta is " + priceWithQuantity);
+	
+	priceWithQuantity = GrubHub.search("Panner",6);
+	System.out.println("The priceWithQuantity of Panner is " + priceWithQuantity);
+	
+	priceWithQuantity = GrubHub.search("Coconut Milk Rice",5);
+	System.out.println("The priceWithQuantity of Coconut Milk Rice is " + priceWithQuantity);
+	
+	priceWithQuantity = GrubHub.search("Pani Puri",4);
+	System.out.println("The priceWithQuantity of Pani Puri is " + priceWithQuantity);
+	
+	
+	priceWithQuantity = GrubHub.search("Egg Masala",6);
+	System.out.println("The priceWithQuantity of Egg Masala is " + priceWithQuantity);
+	
+	priceWithQuantity = GrubHub.search("Butter Naan ",8);
+	System.out.println("The priceWithQuantity of Butter Naan is " + priceWithQuantity);
+	
+	priceWithQuantity = GrubHub.search("Biriyani Rice",4);
+	System.out.println("The priceWithQuantity of Biriyani Rice is " + priceWithQuantity);
+	
+	priceWithQuantity = GrubHub.search("Beetrot Chutney",3);
+	System.out.println("The priceWithQuantity of  Beetrot Chutney is " + priceWithQuantity);
+	
+	priceWithQuantity = GrubHub.search("Fride Rice",6);
+	System.out.println("The priceWithQuantity of  Fride Rice is " + priceWithQuantity);
+	
+	priceWithQuantity = GrubHub.search("Beetrot Curry",7);
+	System.out.println("The priceWithQuantity of Beetrot Curry is " + priceWithQuantity);
+	
+	priceWithQuantity = GrubHub.search("Curried Cauliflower",8);
+	System.out.println("The priceWithQuantity of Curried Cauliflower is  " + priceWithQuantity);
+	
+	priceWithQuantity = GrubHub.search("Parota",8);
+	System.out.println("The priceWithQuantity of Paraota is " + priceWithQuantity);
+	
+	priceWithQuantity = GrubHub.search("Egg Khlapuri",4);
+	System.out.println("The priceWithQuantity of Egg Khlapuri is " + priceWithQuantity);
+	
+	priceWithQuantity = GrubHub.search("Egg DK",5);
+	System.out.println("The priceWithQuantity of Egg DK is " + priceWithQuantity);
+	
+	priceWithQuantity = GrubHub.search("Egg Fry",4);
+	System.out.println("The priceWithQuantity of Egg Fry is " + priceWithQuantity);
+	
+	priceWithQuantity = GrubHub.search("Kaju Kholapuri",7);
+	System.out.println("The priceWithQuantity of Kaju Kholapuri is " + priceWithQuantity);
+	
+	priceWithQuantity = GrubHub.search("Kaju Masala",8);
+	System.out.println("The priceWithQuantity of Kaju Masala is " + priceWithQuantity);
+	
+	priceWithQuantity = GrubHub.search("Ragi Mudde",7);
+	System.out.println("The priceWithQuantity of Ragi Mudde is " + priceWithQuantity);
+	
+	priceWithQuantity = GrubHub.search("EggLess Chocalate Cake",2);
+	System.out.println("The priceWithQuantity of EggLess Chocalate Cake is " + priceWithQuantity);
+	
+	priceWithQuantity = GrubHub.search("Chicken Biriyani",3);
+	System.out.println("The priceWithQuantity of Chicken Biriyani  is " + priceWithQuantity);
+	
+	priceWithQuantity = GrubHub.search("Pizza",3);
+	System.out.println("The priceWithQuantity of Pizza is  " + priceWithQuantity);
+	
+	priceWithQuantity = GrubHub.search("Veg Salada",4);
+	System.out.println("The priceWithQuantity of Veg Salada is " + priceWithQuantity);
+	
+	
+	}
+}
